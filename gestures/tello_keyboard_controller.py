2a0b71c42ce831f118a36a086304d26fc73df58f
erseco <erseco@gmail.com>
2022-11-17 13:31:30 +0000

    Use arrows

--- gestures/tello_keyboard_controller.py
+++ gestures/tello_keyboard_controller.py
@@ -3,15 +3,22 @@ from djitellopy import Tello
 class TelloKeyboardController:
     def __init__(self, tello: Tello):
         self.tello = tello
-
+    
     def control(self, key):
-        if key == ord('w'):
+
+        Upkey = 2490368
+        DownKey = 2621440
+        LeftKey = 2424832
+        RightKey = 2555904    
+
+
+        if key == ord('w') or key == Upkey:
             self.tello.move_forward(30)
-        elif key == ord('s'):
+        elif key == ord('s') or key == DownKey:
             self.tello.move_back(30)
-        elif key == ord('a'):
+        elif key == ord('a') or key == LeftKey:
             self.tello.move_left(30)
-        elif key == ord('d'):
+        elif key == ord('d') or key == RightKey:
             self.tello.move_right(30)
         elif key == ord('e'):
             self.tello.rotate_clockwise(30)
